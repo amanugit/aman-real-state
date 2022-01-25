ee2f23165838fe50a9ac5de92652cbe369895716
Amanuel <amannew49@gmail.com>
2022-01-26 00:06:39 +0300

    this

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 const contentful = require("contentful");
 
 export default contentful.createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  space: process.env.REACT_APP_API_SPACE || "2w3d3q4ck9sv",
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN || "MqUtzZxVrCBeR3-sMKAaUeNFoMbGilKQ7U_i0_TNo84"
 });
